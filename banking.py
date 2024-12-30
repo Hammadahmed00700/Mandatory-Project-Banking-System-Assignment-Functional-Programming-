c6618244dd2fb9a8d2076f0e990e596e85f22d91
Hammad Ahmed <hammad0007000@gmail.com>
2024-12-31 03:03:45 +0500

    create function for deposit and withdraw
    made deposit and withdrawal function. Then,i will make function for check balance and transaction statement

--- banking.py
+++ banking.py
@@ -8,3 +8,23 @@ def create_acc(data):
             }
         print(f"Account for {name} has been create.")
         return data
+def money_dep(data):
+    amount = float(input("Enter Your Amount: "))
+    if amount <= 0:
+            print("Invalid! Amount should be positive.")
+    else:
+            data["balance"] += amount
+            data["transaction"].append(f"Deposit: +{amount:.2f}")
+            print(f"Deposited: {amount:.2f} successfully. Total Amount: {data['balance']:.2f}")
+    return data
+def money_withdraw(data):
+        amount=float(input("Enter Your Amount:"))
+        if amount <=0:
+            print("Invalid!Amount Should Be Positive")
+        if amount >data['balance']:
+            print("Insufficient Funds")
+        else:
+            data["balance"]-=amount
+            data["transaction"].append((f"Withdraw: -{amount:.2f}"))
+            print(f"Withdraw:{amount:.2f} Sucessfully.Total Amount:{data['balance']:.2f}")
+        return data
